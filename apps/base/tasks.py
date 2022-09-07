d7783a40fa51977169ddf2f9b37145fb8c686859
hoou <1084502012@qq.com>
2022-09-07 23:33:10 +0800

    update

--- apps/base/tasks.py
+++ apps/base/tasks.py
@@ -1,12 +1,6 @@
-from celery import shared_task
 from utils.fastmail import send_fast_mail
 
 
 def send_register_email(register_url, email):
     """发送注册邮件"""
     send_fast_mail([email], content="账户激活地址: {}".format(register_url))
-
-
-@shared_task()
-def hello_world():
-    return "hello world"
